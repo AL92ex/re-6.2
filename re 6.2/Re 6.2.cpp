315a374ce26d1bcd7815fd2f28cfe1e39c8abeb2
赵青 <15221421290@163.com>
2016-12-25 13:32:28 +0800

    found

---
+++ re 6.2/Re 6.2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int ratingcounters[11], i, response;
+
+	for (i = 1; i <= 10; ++i)
+		ratingcounters[i] = 0;
+
+	printf("Enter your responses\n");
+
+	if (i !=999)
+
+}
