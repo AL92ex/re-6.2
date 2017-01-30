0375924ecbe80d45530f2b2f7be812f557c49a7c
赵青 <15221421290@163.com>
2017-01-30 14:45:59 +0800

    finish

--- re 6.2/Re 6.2.cpp
+++ re 6.2/Re 6.2.cpp
@@ -3,13 +3,27 @@
 
 int main(void)
 {
-	int ratingcounters[11], i, response;
+	int ratingcounters[11], i, response=0;
 
 	for (i = 1; i <= 10; ++i)
 		ratingcounters[i] = 0;
 
 	printf("Enter your responses\n");
 
-	if (i !=999)
+while (response != 999){
+	scanf("%i", &response);
+		if (response < 1 || response>10)
+			printf("Bad response: %i\n", response);
+		else
+			++ratingcounters[response];
+}
 
+printf("\n\nRating   Number of Responses\n");
+printf("------   -------------------\n");
+
+for (i = 1; i < 10; ++i)
+	printf("%4i%14i\n", i, ratingcounters[i]);
+
+system("pause");
+return 0;
 }
